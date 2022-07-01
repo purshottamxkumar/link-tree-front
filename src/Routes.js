c6cd77175edefb3da73016fd11588ee76eadc44a
Himanshu <himanshu32288@gmail.com>
2022-07-01 22:30:03 +0530

    bug

--- src/Routes.js
+++ src/Routes.js
@@ -8,8 +8,8 @@ import {
 import AdminPage from "./components/Admin/AdminPage";
 import AdminRoutes from "./components/Admin/AdminRoutes";
 import UserPage from "./components/UserPage/UserPage";
-import Auth from "./components/Login";
-import Login from "./components/Login/Login";
+import Auth from "./components/Login/index";
+import Login from "./components/Login/login";
 
 const Routes = () => {
   return (
