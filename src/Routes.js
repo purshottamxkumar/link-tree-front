702a73f43c1b891a88d05e6d7e7b55a39616ce46
Himanshu <himanshu32288@gmail.com>
2022-07-01 23:48:58 +0530

    check deploy

--- src/Routes.js
+++ src/Routes.js
@@ -9,7 +9,6 @@ import AdminPage from "./components/Admin/AdminPage";
 import AdminRoutes from "./components/Admin/AdminRoutes";
 import UserPage from "./components/UserPage/UserPage";
 import Auth from "./components/Login/index";
-import Login from "./components/Login/login";
 
 const Routes = () => {
   return (
@@ -19,12 +18,9 @@ const Routes = () => {
           <Route path="/" exact element={<Auth />} />
           <Route path="/user" exact element={<UserPage />} />
           <Route path=":path" element={<AdminRoutes />} />
-          <Route />
-          <Route path="/" exact element={<UserPage />} />
           <Route path="/admin" exact element={<AdminPage />}>
             <Route path=":path" element={<AdminRoutes />} />
           </Route>
-          <Route path="/login" exact element={<Login />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Switch>
       </Router>
--- src/components/UserPage/SocialLink.js
+++ src/components/UserPage/SocialLink.js
@@ -1,17 +1,25 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import "./SocialLink.css";
 const SocialLink = (props) => {
   return (
     <>
       <div className="icons">
-        <a href="" id='anchor-links'><FacebookIcon className="social-media-icon" /></a>
-        <a href="" id='anchor-links'><TwitterIcon className="social-media-icon" /></a>
-        <a href="" id='anchor-links'><LinkedInIcon className="social-media-icon" /></a>
-        <a href="" id='anchor-links'><InstagramIcon className="social-media-icon" /></a>
+        <a href="/" id="anchor-links">
+          <FacebookIcon className="social-media-icon" />
+        </a>
+        <a href="/" id="anchor-links">
+          <TwitterIcon className="social-media-icon" />
+        </a>
+        <a href="/" id="anchor-links">
+          <LinkedInIcon className="social-media-icon" />
+        </a>
+        <a href="/" id="anchor-links">
+          <InstagramIcon className="social-media-icon" />
+        </a>
       </div>
     </>
   );
